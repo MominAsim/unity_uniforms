7caf012dafbda35802155414b7ec65337da8502f
MominAsim <127621235+MominAsim@users.noreply.github.com>
2024-01-02 21:09:33 +0500

    W

--- src/js_files/Home.js
+++ src/js_files/Home.js
@@ -9,13 +9,13 @@ return (
      <header>
       <h1 id="unityuniformsheaderid" className='unityuniformsheaderclass'>Unity Uniforms</h1> 
       <div id="buttonsheader">
-      <Link className="allbuttons" id="instagram" to="https://www.instagram.com/" target="_blank">INSTAGRAM</Link>
+      <Link className="allbuttons" id="aboutus_id_header" to="/Footer">ABOUT US</Link>
       <Link className="allbuttons" id="loginid" to="/Login">LOGIN</Link>
       <Link className="allbuttons" id="registerid" to="/Register">REGISTER</Link>
       </div>
     </header>    
     <body>
-      <h1 id="school_heading_body"><u><b>Select A School :</b></u></h1>
+      <h1 id="school_heading_body"><b>Select A School </b></h1>
     </body>
  </>
   );
