5b73dcf3c446858d108e0f70e6462c1ede3a3f8b
MominAsim <127621235+MominAsim@users.noreply.github.com>
2024-01-04 15:08:24 +0500

    w

--- src/js_files/Home.js
+++ src/js_files/Home.js
@@ -9,7 +9,7 @@ return (
      <header>
      <Link to="/"><h1 id="unityuniformsheaderid" className='unityuniformsheaderclass' to="/City">Unity Uniforms</h1></Link>
       <div id="buttonsheader">
-      <Link className="allbuttons" id="aboutus_id_header" to="/Footer">ABOUT US</Link>
+      <a className="allbuttons" id="aboutus_id_header" href="#aboutus_div_id">ABOUT US</a>
       <Link className="allbuttons" id="loginid" to="/Login">LOGIN</Link>
       <Link className="allbuttons" id="registerid" to="/Register">REGISTER</Link>
       </div>
@@ -17,6 +17,9 @@ return (
 
     {/* body of home page  */}
     <body>
+      <img src="/images/back_to_school.webp" alt="Get Back To School" className="get_back_to_school_image"/>
+      <br></br>
+      <br></br>
       <h1 id="school_heading_body"><b><u>Select A School</u></b></h1>
       <div className="schoolselect_home_div">
       <Link className="schoolselect_home" id="city_school_select_home" to="/City">
@@ -32,7 +35,27 @@ return (
       <img src="/images/tis.png" alt="Tis" />
       </Link>
       </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </body>
+    <footer>
+    <div className="footer_headings_div">
+    <h2 className="contactus_footer">Contact Us</h2>
+    <h2 className="aboutus_h1">ABOUT US</h2>  
+    </div>
+      <div id="aboutus_div_id" className="aboutus_div_class">
+       <p className="contactus_para">
+        <p className="phone_number">0335 8233366</p>
+       </p>
+       <p className="aboutus_class_para">Unity Uniforms is a provider of quality uniforms in Pakistan. Our commitment to excellence has made us the go-to choice for leading schools, colleges, and all kinds of uniforms. With years of experience in the industry, we have perfected the art of crafting uniforms that are both comfortable and durable.
+       We use the finest materials to ensure our uniforms meet the highest standards. Our team works closely with schools and colleges to create custom uniform solutions that meet their specific needs. From design to delivery, we take great pride in every aspect of our work. Choose Unity Uniforms for a uniform solution that is both reliable and of top-notch quality.</p>
+      </div>
+      </footer>
  </>
   );
 };
