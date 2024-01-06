00655ba053d4255e62b45e6d8ab9285854130bf8
MominAsim <127621235+MominAsim@users.noreply.github.com>
2024-01-07 02:25:28 +0500

    w

--- src/App.js
+++ src/App.js
@@ -4,19 +4,12 @@ import React from "react";
 import Login from "./js_files/Login";
 import Register from './js_files/Register';
 import Home from './js_files/Home';
-import City from './js_files/City';
-import Frobles from './js_files/Frobles';
-import Licium from './js_files/Licium';
 import Tis from './js_files/Tis';
 
-import Cap from './products_js/Cap';
-import Hoodie from './products_js/Hoodie';
+// products
 import Pant from './products_js/Pant';
 import Shirt from './products_js/Shirt';
-import Socks from './products_js/Socks';
 import Sportsshirt from './products_js/Sportshirt';
-import Sportstrousers from './products_js/Sportstrousers';
-import Tie from './products_js/Tie';
 
 const App = () => {
   return (
@@ -26,20 +19,12 @@ const App = () => {
            <Route path='/' element={<Home />} />
            <Route path='Login' element={<Login />} />
            <Route path='Register' element={<Register />} />
-           <Route path='City' element={<City />} />
-           <Route path='Frobles' element={<Frobles />} />
-           <Route path='Licium' element={<Licium />} />
            <Route path='Tis' element={<Tis />} />
 
           {/* actual products */}
-          <Route path='Cap' element={<Cap />} />
-          <Route path='Hoodie' element={<Hoodie />} />
           <Route path='Pant' element={<Pant />} />
           <Route path='Shirt' element={<Shirt />} />
-          <Route path='Socks' element={<Socks />} />
           <Route path='Sportshirt' element={<Sportsshirt />} />
-          <Route path='Sportstrousers' element={<Sportstrousers />} />
-          <Route path='Tie' element={<Tie />} />
         </Routes>
       </BrowserRouter> 
 </div>
--- src/js_files/Home.js
+++ src/js_files/Home.js
@@ -22,15 +22,6 @@ return (
       <br></br>
       <h1 id="school_heading_body"><b><u>Select A School</u></b></h1>
       <div className="schoolselect_home_div">
-      <Link className="schoolselect_home" id="city_school_select_home" to="/City">
-      <img src="/images/tcs.png" alt="City School" />
-      </Link>
-      <Link className="schoolselect_home" id="frobles_select_home" to="/Frobles">
-      <img src="/images/frobles.jpg" alt="Frobles" />
-      </Link>
-      <Link className="schoolselect_home" id="licium_select_home" to="Licium">
-      <img src="/images/lyceum.jpg" alt="Licium" />
-      </Link>
       <Link className="schoolselect_home" id="tis_select_home" to="/Tis">
       <img src="/images/tis.png" alt="Tis" />
       </Link>
