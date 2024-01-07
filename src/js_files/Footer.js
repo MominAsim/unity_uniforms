b0649951d45c43789d7900aedff9b46ccb412198
MominAsim <127621235+MominAsim@users.noreply.github.com>
2024-01-07 19:05:55 +0500

    w

--- src/js_files/Footer.js
+++ src/js_files/Footer.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import './App.css';
 
 const Footer = () => {
 return (
     <footer>
+     <div className="footer_all_info">
     <div className="footer_headings_div">
     <h2 className="contactus_footer">Contact Us</h2>
     <h2 className="aboutus_h1">ABOUT US</h2>  
@@ -15,6 +15,7 @@ return (
        </p>
        <p className="aboutus_class_para">Unity Uniforms is a provider of quality uniforms in Pakistan. Our commitment to excellence has made us the go-to choice for leading schools, colleges, and all kinds of uniforms. With years of experience in the industry, we have perfected the art of crafting uniforms that are both comfortable and durable.
        We use the finest materials to ensure our uniforms meet the highest standards. Our team works closely with schools and colleges to create custom uniform solutions that meet their specific needs. From design to delivery, we take great pride in every aspect of our work. Choose Unity Uniforms for a uniform solution that is both reliable and of top-notch quality.</p>
+      </div>
       </div>
       </footer>
   );
--- src/products_js/Sportshirt.js
+++ src/products_js/Sportshirt.js
@@ -1,24 +1,15 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import './App.css';
+import Header from "../js_files/Header";
+import Footer from "../js_files/Footer";
 
 const Sportshirt = () => {
 return (
  <>
-      {/*header */}
-      <header>
-     <Link to="/"><h1 id="unityuniformsheaderid" className='unityuniformsheaderclass' to="/City">Unity Uniforms</h1></Link>
-      <div id="buttonsheader">
-      <a className="allbuttons" id="aboutus_id_header" href="#aboutus_div_id">ABOUT US</a>
-      <Link className="allbuttons" id="loginid" to="/Login">LOGIN</Link>
-      <Link className="allbuttons" id="registerid" to="/Register">REGISTER</Link>
-      </div>
-    </header>
-
-    {/* body */}
+      <Header />
     <body>
       <div className="all_elements_products_show_page">
-      <img className="logo_second_page" src="/images/tcs.png" alt="Img didnt load :("/><br></br>
+      <img className="logo_second_page" src="/images/tis.png" alt="Img didnt load :("/><br></br>
       <br></br>
       <img className="image_product_secondpage" src="/images/products/Sports_shirt.jpg" alt="Shirt pic" />
       <h1 className="product_name_second_page">Sports shirt</h1>
@@ -38,27 +29,13 @@ return (
       <button className="addtocartbutton">ADD TO CART</button>
       </div>
     </body>
-
-    {/* footer */}
     <br></br>
     <br></br>
     <br></br>
     <br></br>
     <br></br>
     <br></br>
-    <footer>
-    <div className="footer_headings_div">
-    <h2 className="contactus_footer">Contact Us</h2>
-    <h2 className="aboutus_h1">ABOUT US</h2>  
-    </div>
-      <div id="aboutus_div_id" className="aboutus_div_class">
-       <p className="contactus_para">
-        <p className="phone_number">0335 8233366</p>
-       </p>
-       <p className="aboutus_class_para">Unity Uniforms is a provider of quality uniforms in Pakistan. Our commitment to excellence has made us the go-to choice for leading schools, colleges, and all kinds of uniforms. With years of experience in the industry, we have perfected the art of crafting uniforms that are both comfortable and durable.
-       We use the finest materials to ensure our uniforms meet the highest standards. Our team works closely with schools and colleges to create custom uniform solutions that meet their specific needs. From design to delivery, we take great pride in every aspect of our work. Choose Unity Uniforms for a uniform solution that is both reliable and of top-notch quality.</p>
-      </div>
-      </footer>
+    <Footer />
  </>
   );
 };
