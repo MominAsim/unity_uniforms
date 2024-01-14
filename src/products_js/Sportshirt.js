0e58a63786abeff83ab2b3bf6987b0baaacfb409
MominAsim <127621235+MominAsim@users.noreply.github.com>
2024-01-14 19:28:17 +0500

    j

--- src/products_js/Sportshirt.js
+++ src/products_js/Sportshirt.js
@@ -4,7 +4,8 @@ import Header from "../js_files/Header";
 import Footer from "../js_files/Footer";
 
 const Sportshirt = () => {
-return (
+// javasript
+  return (
  <>
     <Header />
     <body>
@@ -14,11 +15,18 @@ return (
       <img className="image_product_secondpage" src="/images/products/sports_shirt/europe/europe_shirt_pic_front.jpg" alt="Shirt pic" />
       <h1 className="product_name_second_page">Sports shirt</h1>
       <p>Sports Shirt for Sports Wear</p>
-      <h2 className="price_product_second_page">RS 500</h2>
+      <h2 className="price_product_second_page">RS 1300</h2>
       <select id="size_selection" name="attribute_pa_size"data-attribute_name="attribute_pa_size" data-show_option_none="yes">
       <option value="Small" class="Small">Small</option>
       <option value="Medium" class="Medium">Medium</option>
       <option value="Large" class="Large">Large</option>
+      </select>
+      <br></br>
+      <br></br>
+      <br></br>
+      <select id="sports_shirt_selector" name="attribute_pa_size"data-attribute_name="attribute_pa_size" data-show_option_none="yes">
+      <option value="Europe" class="Europe_sports_shirt_selector_option">Europe</option>
+      <option value="Africa" class="Africa_sports_shirt_selector_option">Africa</option>
       </select>
       <br></br>
       <br></br>
